bed07219837e85a207c99da959ba980282873f42
David Wolinsky <david.wolinsky@venturetec.net>
2022-11-29 19:08:34 -0500

    more practice

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -21,16 +21,16 @@ class News_model extends CI_Model {
 
     public function set_news()
     {
-    $this->load->helper('url');
+        $this->load->helper('url');
 
-    $slug = url_title($this->input->post('title'), 'dash', TRUE);
+        $slug = url_title($this->input->post('title'), 'dash', TRUE);
 
-    $data = array(
-        'title' => $this->input->post('title'),
-        'slug' => $slug,
-        'text' => $this->input->post('text')
+        $data = array(
+            'title' => $this->input->post('title'),
+            'slug' => $slug,
+            'body' => $this->input->post('text')
     );
 
-    return $this->db->insert('news', $data);
+        return $this->db->insert('news', $data);
     }
 }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,12 @@
 <html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
 
-                <h1><?php echo $title; ?></h1>
+<head>
+        <title>CodeIgniter Tutorial</title>
+</head>
+
+<body>
+
+        <h1><?php echo $title; ?></h1>
+
+        <p><a href="<?php echo site_url('news/'); ?>">All News</a></p>
+        <p><a href="<?php echo site_url('news/create/'); ?>">Create News</a></p>
